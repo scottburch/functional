36e822978f0afbbc7e49db878782cd8ff38125da
Scott Burch <scott@bulldoginfo.com>
2014-12-10 13:43:25 -0800

    changed promise to try to get it to work on jsfiddle

--- src/promise.js
+++ src/promise.js
@@ -1,32 +1,30 @@
-(function () {
-    F.Promise = F.MONAD(function (monad, initialValue) {
-        var value;
-        var cb;
-        var chainPromise;
-        monad.isPromise = true;
+F.Promise = F.MONAD(function (monad, initialValue) {
+    var value;
+    var cb;
+    var chainPromise;
+    monad.isPromise = true;
 
-        monad.bind = function (fn) {
-            if (value === undefined) {
-                cb = fn;
-                chainPromise = F.Promise();
-                return chainPromise;
-            } else {
-                return fn(value);
-            }
-        };
+    monad.bind = function (fn) {
+        if (value === undefined) {
+            cb = fn;
+            chainPromise = F.Promise();
+            return chainPromise;
+        } else {
+            return fn(value);
+        }
+    };
 
-        monad.resolve = function (v) {
-            value = v;
-            if (cb) {
-                var mValue = cb(v);
-                if (chainPromise && mValue) {
-                    var cbValue;
-                    mValue.bind(function (v) {
-                        cbValue = v;
-                    });
-                    chainPromise.resolve(cbValue);
-                }
+    monad.resolve = function (v) {
+        value = v;
+        if (cb) {
+            var mValue = cb(v);
+            if (chainPromise && mValue) {
+                var cbValue;
+                mValue.bind(function (v) {
+                    cbValue = v;
+                });
+                chainPromise.resolve(cbValue);
             }
-        };
-    });
-}());
+        }
+    };
+});
